e7b6192351e260e3311c0f15ce9c91c74d96a853
shazaanphansofkar <shazaanphansofkar@gmail.com>
2023-07-04 18:21:42 +0200

    Control alignment with click

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,16 @@
 import { Picture } from "../../components";
 import "./card.css";
 
-const Card = ({ title, text, imgSrc, position = "top" }: any) => {
+const Card = ({ title, text, imgSrc, imgPosition }: any) => {
   return (
-    <div className={`card card-with-image-${position}`}>
+    <div className={`card card-with-image-${imgPosition}`}>
       <div className="card-image">
         <Picture imgSrc={imgSrc} />
       </div>
 
       <div className="card-content">
         <h3 className="title">{title}</h3>
-        <p className="text">{text}</p>
+        <p className="text">{text}</p>       
       </div>
     </div>
   );
